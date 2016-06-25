c5649eb00f309cc82a19c6a71f340a109743bdac
Megh Parikh <meghprkh@gmail.com>
2016-06-25 22:39:34 +0530

    Fix server requests and setup the project for production

--- src/js/superagent-setup.js
+++ src/js/superagent-setup.js
@@ -1,11 +1,11 @@
 import request from 'superagent-use'
 
 var prefix;
-if (DEV) prefix = require('superagent-prefix')('http://localhost:3000');
+if (DEV) prefix = require('superagent-prefix')('http://localhost:3000/api');
 else prefix = require('superagent-prefix')('/api');
 
 request.use(prefix)
 request.use(req => req.type('form'))
-request.use(req => req.set('Authorization', authToken))
+request.use(req => authToken && req.set('Authorization', authToken))
 
 window.request = request
